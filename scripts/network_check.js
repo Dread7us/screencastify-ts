f155cf8f4bd32f7c6e33f8b41464cc6ebd56bbdb
Kerrick-Castify <101149213+Kerrick-Castify@users.noreply.github.com>
2022-04-21 08:10:25 -0700

    Update network_check.js

--- scripts/network_check.js
+++ scripts/network_check.js
@@ -237,7 +237,7 @@ function download(content, fileName, contentType) {
 // Listen for click in <body> then download the file
 document.getElementById("click_test").addEventListener("click", function() {
     var scrape = document.documentElement.innerHTML;
-    download(JSON.stringify(scrape), "screencastify-ts.html", "text/plain");
+    download(scrape, "screencastify-ts.html", "text/plain");
 }, false);
 
 get_IP();
