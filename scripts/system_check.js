50dde0922db9a139dff709fde635515f57a0a867
Kerrick-Castify <101149213+Kerrick-Castify@users.noreply.github.com>
2022-04-28 13:45:22 -0700

    Update system_check.js

--- scripts/system_check.js
+++ scripts/system_check.js
@@ -232,7 +232,7 @@ document.getElementById("click_test").addEventListener("click", function () {
         download(blob, "screencastify-ts-" + timestamp + ".jpg", "image/jpeg");
       });
     });
-  reff = document.getElementById("speedtest").contentDocument.referrer;
+  reff = document.getElementById("speedtest").contentDocument;
   var value = $('#speedtest').contents().find('#rect').text();
   console.log("Value: " + value + "Referrer: " + reff);
   }, false
