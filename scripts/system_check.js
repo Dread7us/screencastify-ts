939fdcda73feb320bae215743bcce1fec2d82366
Kerrick-Castify <101149213+Kerrick-Castify@users.noreply.github.com>
2022-04-29 15:07:13 -0700

    Update system_check.js

--- scripts/system_check.js
+++ scripts/system_check.js
@@ -252,3 +252,22 @@ user_label.innerHTML = navigator.userAgent;
 for (var i = 0; i < web_pages.length; i++) {
    checkURL(web_pages[i], i);
 }
+
+const NetworkSpeed = require(['network-speed']);
+
+async function getNetworkUploadSpeed() {
+  const options = {
+    hostname: 'www.google.com',
+    port: 80,
+    path: '/catchers/544b09b4599c1d0200000289',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  const fileSizeInBytes = 2000000
+  const speed = await testNetworkSpeed.checkUploadSpeed(options, fileSizeInBytes);
+  console.log(speed);
+}
+
+getNetworkUploadSpeed();
