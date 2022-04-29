3ae2730ef5a4d1d83538ed3411f79944d4c4e043
Kerrick-Castify <101149213+Kerrick-Castify@users.noreply.github.com>
2022-04-29 14:41:43 -0700

    Update system_check.js

--- scripts/system_check.js
+++ scripts/system_check.js
@@ -252,32 +252,3 @@ user_label.innerHTML = navigator.userAgent;
 for (var i = 0; i < web_pages.length; i++) {
    checkURL(web_pages[i], i);
 }
-
-const FastSpeedtest = require(['fast-speedtest-api']);
-   
-const myPromise = fastnetApi();
-myPromise.then(res =>{
-  console.log('it returned:', res);
-  return res 
-})
-
-function fastnetApi() {
-
-    let speedtest = new FastSpeedtest({
-        token: "YXNkZmFzZGxmbnNkYWZoYXNkZmhrYWxm", //**** required
-        verbose: false, // default: false
-        timeout: 5000, // default: 5000
-        https: true, // default: true
-        urlCount: 5, // default: 5 
-        bufferSize: 8, // default: 8 
-        unit: FastSpeedtest.UNITS.Mbps // default: Bps
-    });
-
-    return speedtest.getSpeed().then(s => {
-        console.log(`Speed: ${s} Mbps`);
-    console.log(typeof s)
-    return s;
-    }).catch(e => {
-        console.error(e.message);
-    });
-}
