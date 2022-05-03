32633bb0227a885471722068644530ba7e1a1ad1
Kerrick-Castify <101149213+Kerrick-Castify@users.noreply.github.com>
2022-05-03 12:31:39 -0700

    Update system_check.js

--- scripts/system_check.js
+++ scripts/system_check.js
@@ -248,7 +248,7 @@ for (var i = 0; i < web_pages.length; i++) {
 
 // Experimental code below; trying to access speed test results (iframe, cors issue)
 
-var url = "https://anyorigin.com/go?url=" + encodeURIComponent("//openspeedtest.com/Get-widget.php?run") + "&callback=?";
-$.get(url, function(response) {
-  console.log(response);
-});
+$.getJSON('https://api.allorigins.win/get?url=' + encodeURIComponent('https://openspeedtest.com/Get-widget.php?run'), function (data) {
+                      alert(data.contents);
+  console.log(data.contents);
+                  });
