dabad8a67feb466f57fd137e0b33b7901e6d6a42
Kerrick-Castify <101149213+Kerrick-Castify@users.noreply.github.com>
2022-05-05 15:12:47 -0700

    Update system_check.js

--- scripts/system_check.js
+++ scripts/system_check.js
@@ -222,10 +222,10 @@ function checkURL(url, which) {
 function changeBorderColor(id) {
   var load = setTimeout(function() {
     //document.getElementById(id).style.border = "1px solid #ddd";
-    document.getElementById(id).className = "inputred";
+    document.getElementById(id).className = "input";
   }, 5000);
   //document.getElementById(id).style.border = "1px solid Red";
-  document.getElementById(id).className = "input";
+  document.getElementById(id).className = "inputred";
 }
 
 // Listen for click on the title of the page then download the file
