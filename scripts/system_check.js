cb5d1d6031bf3fcb88d9ab80e3c1cb9015fc77c1
Kerrick-Castify <101149213+Kerrick-Castify@users.noreply.github.com>
2022-07-22 16:14:47 -0700

    Update system_check.js

--- scripts/system_check.js
+++ scripts/system_check.js
@@ -299,8 +299,8 @@ document.getElementById("click_test").addEventListener(
         document.getElementById("click_test").style.display="none";
         document.getElementById("help").style.display="none";
         document.getElementById("restart").style.display="none";
-        document.getElementByClass("hr-bottom").style.display="none";
-        document.getElementByClass("hr-system-info").style.display="none";
+        document.getElementsByClassName("hr-bottom").style.display="none";
+        document.getElementsByClassName("hr-system-info").style.display="none";
         
         if (failed_webpages.length > 0) {
           var failed_list = "";
