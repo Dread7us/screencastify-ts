f14b9b4fad164d6b2d4a490ff505c745105634b5
Kerrick-Castify <101149213+Kerrick-Castify@users.noreply.github.com>
2022-11-17 12:00:09 -0800

    Update system_check.js

--- scripts/system_check.js
+++ scripts/system_check.js
@@ -144,6 +144,7 @@ web_pages[0] = new Array ("https://accounts.google.com",
                           "https://clients2.google.com/service/update2/crx");
 web_pages[1] = new Array ("https://apis.google.com/js/api.js");
 web_pages[2] = new Array ("https://app.castify.com",
+                          "https://screencastify.com",
                           "https://studio-backend.castify.com",
                           "https://studio-graphql.castify.com",
                           "https://app.screencastify.com", 
