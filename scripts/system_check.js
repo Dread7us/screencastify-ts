3a434a42b209935278f52f232d210395cb46ffde
Kerrick-Castify <101149213+Kerrick-Castify@users.noreply.github.com>
2024-09-03 09:21:54 -0700

    Update system_check.js
    Added new required domain: "https://identitytoolkit.googleapis.com"

--- scripts/system_check.js
+++ scripts/system_check.js
@@ -40,6 +40,7 @@ web_pages[4] = new Array ("https://firestore.googleapis.com",
                           "https://firebasestorage.googleapis.com", 
                           "https://firebase.googleapis.com/v1beta1/availableProjects",
                           "https://storage.googleapis.com",
+                          "https://identitytoolkit.googleapis.com",
                           "https://googleapis.com/drive/v2/files");
 web_pages[5] = new Array ("https://lh3.googleusercontent.com/favicon.ico");
 web_pages[6] = new Array ("https://pendo.io",
